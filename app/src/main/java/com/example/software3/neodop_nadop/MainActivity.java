85cae775f8a20669f48ab518a429e8b031cdb70b
ray5273 <ray5273@naver.com>
2018-05-01 16:54:15 +0900

    initial commit

---
+++ app/src/main/java/com/example/software3/neodop_nadop/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.software3.neodop_nadop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

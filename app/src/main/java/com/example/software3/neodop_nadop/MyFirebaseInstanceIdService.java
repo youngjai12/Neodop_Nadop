ab884ccb09c7d537365cc1193e76d0e21e77bac5
BlindedShooter <gkadnflh@gmail.com>
2018-05-14 20:12:30 +0900

    Added Firebase token registration

--- app/src/main/java/com/example/software3/neodop_nadop/MyFirebaseInstanceIdService.java
+++ app/src/main/java/com/example/software3/neodop_nadop/MyFirebaseInstanceIdService.java
@@ -5,6 +5,15 @@ import android.util.Log;
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.FirebaseInstanceIdService;
 
+import java.io.BufferedWriter;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class MyFirebaseInstanceIdService extends FirebaseInstanceIdService{
     private static final String TAG = "FCM";
 
@@ -12,11 +21,40 @@ public class MyFirebaseInstanceIdService extends FirebaseInstanceIdService{
     public void onTokenRefresh() {
         // Get updated InstanceID token.
         String refreshedToken = FirebaseInstanceId.getInstance().getToken();
-        Log.d(TAG, "Refreshed token: " + refreshedToken);
+        Log.d(TAG, "Refreshed token1: " + refreshedToken);
 
         // If you want to send messages to this application instance or
         // manage this apps subscriptions on the server side, send the
         // Instance ID token to your app server.
-        //      sendRegistrationToServer(refreshedToken);
+        Log.d(TAG, "sending token to server");
+        sendRegistrationToServer(refreshedToken);
+    }
+
+    private void sendRegistrationToServer(String token) {
+        try {
+            URL url = new URL("http://neodop-nadop.iptime.org/store");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+            connection.setDoOutput(true);
+            connection.setDoInput(true);
+
+            connection.setRequestMethod("POST");
+
+            DataOutputStream dos = new DataOutputStream(connection.getOutputStream());
+
+            dos.writeBytes("token=" + token);
+
+            connection.connect();
+
+            if (connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                // Do whatever you want after the
+                // token is successfully stored on the server
+            }
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
